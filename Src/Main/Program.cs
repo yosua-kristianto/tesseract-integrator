d49bf0c16f9aae804e09ad17ddf8779d6890940c
Yosua Kristianto <yosua_kristianto144@outlook.com>
2023-09-21 17:46:00 +0700

    Add project files.

---
+++ Src/Main/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using Tesseract;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(AppContext.BaseDirectory);
+
+        string imagePath = "D:\\Code\\College\\Thesis Research\\CRNN Experiments\\thesis-thingy\\Src\\Resources\\Image\\2.png";
+        string tesseractFeederPath = "D:\\Code\\College\\Thesis Research\\CRNN Experiments\\thesis-thingy\\Src\\Resources\\Data\\";
+
+        TesseractEngine model = new TesseractEngine(tesseractFeederPath, "eng", EngineMode.Default);
+        Pix loadedImage = Pix.LoadFromFile(imagePath);
+
+        Page page = model.Process(loadedImage);
+
+        string text = page.GetText();
+
+        Console.WriteLine("Detected Text: " + text);
+    }
+}
