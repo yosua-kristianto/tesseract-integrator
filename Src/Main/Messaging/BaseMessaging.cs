b564475d4e86e6a66ec2079ef433ce9ecd2c0ed0
Yosua Kristianto <yosua_kristianto144@outlook.com>
2023-10-02 00:22:06 +0700

    Added BaseMessaging and OCRProcessorConsumer

---
+++ Src/Main/Messaging/BaseMessaging.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tesseract_integrator.Src.Main.Facade;
+
+namespace tesseract_integrator.Src.Main.Messaging
+{
+    public abstract class BaseMessaging
+    {
+        protected string groupId { get; set; }
+        protected string server { get; set; }
+
+        public BaseMessaging(IConfiguration config)
+        {
+            this.groupId = KafkaConfigConstant.KAFKA_DEFAULT_GROUP_ID;
+            this.server = config.GetValue<string>("KafkaServer") ?? "localhost:9092";
+        }
+    }
+}
---
+++ Src/Main/Messaging/Consumer/OCRProcessorConsumer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tesseract_integrator.Src.Main.Messaging.Consumer
+{
+    public class OCRProcessorConsumer: BaseMessaging
+    {
+        public static string TOPIC = "IMAGE_OCR_OPERATION_TOPIC";
+
+        public OCRProcessorConsumer(IConfiguration config): base(config){}
+
+    }
+}
