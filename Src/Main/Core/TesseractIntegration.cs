81125cb8a046b6fc1432aaeff76ef236d5c08f5e
Yosua Kristianto <yosua_kristianto144@outlook.com>
2023-10-02 00:21:52 +0700

    Completed TesseractIntegration core engine

---
+++ Src/Main/Core/TesseractIntegration.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Tesseract;
+using tesseract_integrator.Src.Main.Facade;
+
+namespace tesseract_integrator.Src.Main.Core
+{
+    /**
+     * TesseractIntegration
+     * @singleton
+     * 
+     * This class contains integration to Tesseract model.
+     */
+    internal class TesseractIntegration
+    {
+        private static TesseractIntegration _instance = null;
+        private string tesseractFeederPath = "";
+        private TesseractEngine tesseractModel;
+
+        private TesseractIntegration() 
+        {
+            this.tesseractModel = new TesseractEngine(
+                this.tesseractFeederPath, 
+                TesseractConstant.TESSERACT_ENGINE_LANGUAGE, 
+                TesseractConstant.TESSERACT_ENGINE_LANGUAGE
+            );
+        }
+
+        public static TesseractIntegration GetInstance()
+        {
+            if(_instance == null)
+            {
+                _instance = new TesseractIntegration();
+            }
+
+            return _instance;
+        }
+
+        /**
+         * Ocr
+         * @public
+         * 
+         * This function will perform the TesseractEngine reading by loading
+         * installed tesseract model, and using it to reverse image to text.
+         * And then will return the string of the result of the reversed image.
+         * 
+         * @param string path
+         *  The path to file.
+         *  
+         * @return string
+         */
+        public string Ocr(string path)
+        {
+            Pix imageToBeLoad = Pix.LoadFromFile(path);
+
+            Page page = this.tesseractModel.Process(imageToBeLoad);
+
+            return page.GetText();
+        }
+    }
+}
---
+++ Src/Main/Facade/Constant.cs
@@ -0,0 +1,24 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tesseract;
+
+namespace tesseract_integrator.Src.Main.Facade
+{
+    // This part contains constant used for the Tesseract.
+    public class TesseractConstant
+    {
+        public static String TESSERACT_ENGINE_LANGUAGE = "eng";
+        public static EngineMode TESSERACT_ENGINE_MODE = EngineMode.Default;
+    }
+
+    // This part contains constant used for Kafka Configuration.
+    public class KafkaConfigConstant
+    {
+        public static String KAFKA_DEFAULT_GROUP_ID = "group-1";
+        public static AutoOffsetReset KAFKA_DEFAULT_AUTO_OFFSET_RESET = AutoOffsetReset.Earliest;
+    }
+}
