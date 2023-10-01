bdf77798b0f94b6bef71e722715196f1e9871bd2
Yosua Kristianto <yosua_kristianto144@outlook.com>
2023-10-02 00:21:34 +0700

    Taking EnvironmentVariables and Logging from Job Scheduler Services

---
+++ Src/Main/Config/EnvironmentVariables.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+
+namespace tesseract_integrator.Config
+{
+    /**
+     * @config
+     * @singleton
+     * EnvironmentVariables
+     * 
+     * This class is the helper for extracting selected values from the appsettings file to 
+     * serialized value for easier accessibility.
+     */ 
+    public class EnvironmentVariables
+    {
+        private static EnvironmentVariables _instance = null;
+        private Dictionary<string, string> keys = new();
+
+        private EnvironmentVariables() { }
+
+        public static EnvironmentVariables Instance(Dictionary<string, string> setupKeys)
+        {
+            if( _instance == null)
+            {
+                _instance = new EnvironmentVariables();
+                _instance.keys = setupKeys;
+            }
+            return _instance;
+        }
+
+        public static EnvironmentVariables GetInstance() => _instance;
+
+        /**
+         * @public 
+         * Key
+         * 
+         * This function used for retrieving registered value within keys by determined key name.
+         */
+        public string Key(string key) 
+        {
+            return this.keys[key];
+        }
+    }
+}
---
+++ Src/Main/Facade/Logging.cs
@@ -0,0 +1,93 @@
+﻿using tesseract_integrator.Config;
+
+namespace tesseract_integrator.Facade
+{
+    /**
+     * Logging
+     * 
+     * This class used for making custom-made log. 
+     * The target of this functionality is writing the log with given format for better text-mining searching. 
+     */
+    public class Log
+    {
+        private static readonly string INFO = "INFO";
+        private static readonly string ERROR = "ERROR";
+
+        // Prevent any instance creation outside this class
+        private Log() { }
+
+        /**
+         * @private
+         * @void
+         * WriteLog
+         * 
+         * This function will directly organize log message to be written in the respective place.
+         * 
+         * This function's algorithm will be executed as below:
+         * 
+         * 1. Get current time with format of Y-m-d H:i:s
+         * 2. Get current time with format of Ymd
+         * 3. Write log with format of [Y-m-d H:i:s] [LOG_TYPE] [Message]
+         * 4. If there is file named as number 2 in destinated folder on "CustomLogRepository" in appsettings.json
+         *      4.1 Append the generated log in number 3 to the file
+         *      4.2 Else, write new file with name as number 2 with format of *.log
+         */
+        private static void WriteLog(string message, string logType)
+        {
+            DateTime currentTime = DateTime.Now;
+            
+            // Call out step 1 & 2
+            string currentTimeInString = currentTime.ToString("yyyy-MM-dd H:m:s.FFFF zzz");
+            string currentDateInString = currentTime.ToString("yyyyMMdd");
+
+            // Call out step 3
+            string logToBeWritten = "[" + currentTimeInString + "]"
+                                        + "[" + logType + "]"
+                                        + message;
+
+            string directoryPath = EnvironmentVariables.GetInstance().Key("default-log-path");
+            string pathToLog = directoryPath + "/" + currentDateInString + ".log";
+
+            FileStream stream;
+            // Call out 4 & 4.1
+            if (!File.Exists(pathToLog)) 
+            {
+                if (!Directory.Exists(directoryPath))
+                {
+                    Directory.CreateDirectory(directoryPath);
+                }
+
+                File.Create(pathToLog);
+            }
+            
+            // I'm expecting that in this step, the log file has been created and ready to be written off
+            File.AppendAllText(pathToLog, logToBeWritten+"\n");
+        }
+
+        /**
+         * @static
+         * @public
+         * @void
+         * I
+         * 
+         * This function will help with writing the INFO type log to the destinated path.
+         */
+        public static void I(string logContent)
+        {
+            Log.WriteLog(logContent, Log.INFO);
+        }
+
+        /**
+         * @static
+         * @public
+         * @void
+         * E
+         * 
+         * This function will help with writing the ERROR type log to the destinated path.
+         */
+        public static void E(string logContent)
+        {
+            Log.WriteLog(logContent, Log.ERROR);
+        }
+    }
+}
